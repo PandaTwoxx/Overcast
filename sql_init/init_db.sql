ae8eb2a86b70afc76b0fa86328c429a014d59f9f
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-12-30 13:04:17 -0500

    Created init sql file

---
+++ sql_init/init_db.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS overcast;
+
+
+CREATE DATABASE overcast;
+
+
+USE overcast;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname varchar(255) NOT NULL,
+    lastname varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    CONSTRAINT unique_username UNIQUE(username),
+    CONSTRAINT unique_email UNIQUE(email)
+);
+
+CREATE TABLE topics(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    topic varchar(255) NOT NULL,
+    description varchar(255),
+    upVotes INT NOT NULL,
+    downVotes INT NOT NULL
+);
+
+CREATE TABLE votes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    topic_id INT NOT NULL,
+    user_id INT NOT NULL,
+    vote BOOLEAN NOT NULL, -- True = UpVote, False = DownVote
+
+    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
