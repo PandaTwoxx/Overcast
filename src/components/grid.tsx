410e027141e074eee803e63db596a844bd5ae7ec
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-01-20 15:17:20 -0500

    fixed component

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 interface GridProps {
   children: React.ReactNode;
-  columns?: string; // Or a more specific type if you have restrictions
-  gap?: string; // Or a more specific type like number or a specific set of strings
+  columns?: string;
+  gap?: string;
   className?: string;
   style?: React.CSSProperties;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any; // Allow other props to be passed through
+  [key: string]: any;
 }
 
 export default function Grid({ children, columns = "1fr 1fr", gap = "4", className, style, ...props }: GridProps) {
