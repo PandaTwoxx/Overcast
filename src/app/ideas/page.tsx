16677da433cc0cdbed6b1b78c3c45c0aa0467779
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-01-20 15:41:48 -0500

    fixed vercel build error

--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -3,10 +3,18 @@ import queryDatabase from '@/api/query';
 export default async function Ideas() {
     const query = 'SELECT * FROM users';
     const rows = await queryDatabase(query);
-    console.log(rows);
     return (
         <>
-            {rows}
+            <div>
+                <h1>Users</h1>
+                <ul>
+                    {rows.rows.map((row, index: number) => (
+                        <li key={index}>
+                            {JSON.stringify(row)} {/* Render each row as a string (or customize the display) */}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     )
 }
