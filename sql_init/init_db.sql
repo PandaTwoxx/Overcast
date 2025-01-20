3ec633b37eae9866158208d526d75d1123b883c5
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-01-20 15:18:12 -0500

    fixed sql to work with pg

--- sql_init/init_db.sql
+++ sql_init/init_db.sql
@@ -1,35 +1,35 @@
-DROP DATABASE IF EXISTS overcast;
-
-
-CREATE DATABASE overcast;
+DROP TABLE votes;
+DROP TABLE users;
+DROP TABLE topics;
 
 
 -- Create the users table
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,  -- Use SERIAL for auto-incrementing integers
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    CONSTRAINT unique_username UNIQUE(username)
+                       id SERIAL PRIMARY KEY,  -- Use SERIAL for auto-incrementing integers
+                       firstname VARCHAR(255) NOT NULL,
+                       lastname VARCHAR(255) NOT NULL,
+                       username VARCHAR(255) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       CONSTRAINT unique_username UNIQUE(username)
 );
 
 -- Create the topics table
 CREATE TABLE topics (
-    id SERIAL PRIMARY KEY,
-    topic VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    upVotes INT NOT NULL DEFAULT 0,  -- Add default values for upVotes and downVotes
-    downVotes INT NOT NULL DEFAULT 0
+                        id SERIAL PRIMARY KEY,
+                        topic VARCHAR(255) NOT NULL,
+                        description VARCHAR(255),
+                        userid INT NOT NULL,
+                        upVotes INT NOT NULL DEFAULT 0,  -- Add default values for upVotes and downVotes
+                        downVotes INT NOT NULL DEFAULT 0
 );
 
 -- Create the votes table
 CREATE TABLE votes (
-    id SERIAL PRIMARY KEY,
-    topic_id INT NOT NULL,
-    user_id INT NOT NULL,
-    vote BOOLEAN NOT NULL, -- True = UpVote, False = DownVote
+                       id SERIAL PRIMARY KEY,
+                       topic_id INT NOT NULL,
+                       user_id INT NOT NULL,
+                       vote BOOLEAN NOT NULL, -- True = UpVote, False = DownVote
 
-    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                       FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
+                       FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
