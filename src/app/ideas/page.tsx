63f1c824d987df1cbf6d91b76ab917c736599cd1
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-02-02 13:46:23 -0500

    Changed ideas ui

--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -1,20 +1,36 @@
-import queryDatabase from '@/api/query';
+import { getUserPost } from "@/api/posts";
+import { getUsername } from "@/api/users";
+import StackedList from '@/components/stackedList';
+import {JSX} from "react/jsx-runtime";
+import IntrinsicAttributes = JSX.IntrinsicAttributes;
+
+
+interface PostData {
+    topic: string;
+    description: string;
+    username: string;
+    imageUrl: string;
+    upvotes: number;
+    downvotes: number;
+}
 
 export default async function Ideas() {
-    const query = 'SELECT * FROM users';
-    const rows = await queryDatabase(query);
+    const rows = (await getUserPost(1)).rows;
+
+    const Posts: (IntrinsicAttributes & PostData)[] = await Promise.all(
+        rows.map(async (row) => ({
+            ...row,
+            username: (await getUsername(row.userid)) as string,
+            imageUrl: '/vercel.svg', // Replace with actual image URL logic
+        }))
+    ) ;
+
     return (
         <>
-            <div>
+            <div className="px-5">
                 <h1>Users</h1>
-                <ul>
-                    {rows.rows.map((row, index: number) => (
-                        <li key={index}>
-                            {JSON.stringify(row)} {/* Render each row as a string (or customize the display) */}
-                        </li>
-                    ))}
-                </ul>
+                <StackedList posts={Posts} />
             </div>
         </>
-    )
+    );
 }
