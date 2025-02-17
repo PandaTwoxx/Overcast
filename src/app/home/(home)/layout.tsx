4523dd7fb9587cc24bea4f7e8840bf973fe59ea2
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-02-17 18:30:37 -0500

    fixed de layouts

--- src/app/home/(home)/layout.tsx
+++ src/app/home/(home)/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+import "../../globals.css";
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import React from 'react';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Overcast Home",
@@ -23,10 +12,10 @@ export const metadata: Metadata = {
 };
 
 const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Posts', href: '#', current: false },
-  { name: 'Votes', href: '#', current: false },
-  { name: 'Ideas', href: '#', current: false },
+    { name: 'Home', href: '/home', current: true },
+    { name: 'Posts', href: '/home/posts', current: false },
+    { name: 'Votes', href: '/home/votes', current: false },
+    { name: 'Ideas', href: '/home/ideas', current: false },
 ]
 
 
@@ -36,16 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
-      >
+      <>
         <Analytics />
         <SpeedInsights />
         <Navbar navigation={navigation}/>
         {children}
         <Footer/>
-      </body>
-    </html>
+      </>
   );
 }
--- src/app/home/ideas/layout.tsx
+++ src/app/home/ideas/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../../globals.css";
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
@@ -7,45 +6,31 @@ import React from 'react';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Overcast Home",
-  description: "Overcasting",
+    title: "Overcast Ideas",
+    description: "Overcasting fish",
 };
 
 const navigation = [
-  { name: 'Home', href: '#', current: false },
-  { name: 'Posts', href: '#', current: false },
-  { name: 'Votes', href: '#', current: false },
-  { name: 'Ideas', href: '#', current: true },
+    { name: 'Home', href: '/home', current: false },
+    { name: 'Posts', href: '/home/posts', current: false },
+    { name: 'Votes', href: '/home/votes', current: false },
+    { name: 'Ideas', href: '/home/ideas', current: true },
 ]
 
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="h-full">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
-      >
-        <Analytics />
-        <SpeedInsights />
-        <Navbar navigation={navigation}/>
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  );
+    return (
+        <>
+            <Analytics />
+            <SpeedInsights />
+            <Navbar navigation={navigation}/>
+            {children}
+            <Footer/>
+        </>
+    );
 }
