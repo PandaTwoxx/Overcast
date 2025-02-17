d98b59dda69fdfb291b9b0fd880d3079974e92c2
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-02-17 18:32:32 -0500

    Added use effect

--- src/app/home/ideas/page.tsx
+++ src/app/home/ideas/page.tsx
@@ -1,10 +1,12 @@
-'use server';
+'use client';
 
 import { getUserPost } from "@/api/posts";
-import { getUsername } from "@/api/users";
+import { getUsername, getUserId } from "@/api/users";
 import StackedList from '@/components/stackedList';
 import { redirect } from 'next/navigation';
 import { auth } from '@/auth';
+import React, {useEffect, useState} from 'react'
+import {User} from "next-auth";
 
 
 interface PostData {
@@ -16,28 +18,56 @@ interface PostData {
     downvotes: number;
 }
 
-export default async function Ideas() {
-    const session = await auth();
-    if (!session?.user) {
-        redirect('/login'); // Or handle unauthenticated state as needed
-    }
-    const user = session.user;
-    const rows = (await getUserPost(Number(user.id))).rows;
+export default function Ideas() {
+    const [user, setUser] = useState<User>({});//await auth();
+    useEffect(() => {
+        const getSessionData = async () => {
+            try {
+                const currentSession = await auth();
 
-    const Posts: (PostData)[] = await Promise.all(
-        rows.map(async (row) => ({
-            ...row,
-            username: (await getUsername(row.userid)) as string,
-            imageUrl: '/vercel.svg', // Replace with actual image URL logic
-        }))
-    ) ;
+                if (!currentSession?.user) {
+                    redirect('/login'); // Or handle unauthenticated state as needed
+                }
+                setUser(currentSession.user);
+            }catch (e){
+                throw e;
+            }
+        }
+        getSessionData().then(r => {console.log(r)});
+    }, [])
+    const [posts, setPosts] = useState<(PostData)[]>([]);
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const rows = (await getUserPost(user.id || await getUserId(user.name || ""))).rows;
+                setPosts(await Promise.all(
+                    rows.map(async (row) => ({
+                        ...row,
+                        username: (await getUsername(row.userid)) as string,
+                        imageUrl: '/vercel.svg', // Replace with actual image URL logic
+                    }))
+                ));
+            } catch (e) {
+                throw e;
+            }
+        }
+
+        getPosts().then(r => {console.log(r)});
+    }, [user.id, user.name]);
 
     return (
         <>
-            <div className="px-5">
-                <h1>Users</h1>
-                <StackedList posts={Posts} />
-            </div>
+            <div className="px-5 relative">
+            <h1>Users</h1>
+            <StackedList posts={posts}/>
+            <button
+                className="absolute bottom-0 right-10 inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                <span
+                    className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                Add Idea
+                </span>
+            </button>
+        </div>
         </>
-    );
+);
 }
