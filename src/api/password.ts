ebc96145fafb334ba3f065ab82af623cf8cdb464
pandathreexx <pandathreexx@gmail.com>
2025-03-02 14:24:41 +0000

    switched to bcrypt js

--- src/api/password.ts
+++ src/api/password.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 export async function hashPassword(plainPassword: string): Promise<string> {
     const saltRounds = 10; // Number of salt rounds for bcrypt
