88b6c9420114fd3083dde5fd3bfe4d3edf9de053
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-04-11 12:54:10 -0400

    added debug message

--- src/components/postsGrid.tsx
+++ src/components/postsGrid.tsx
@@ -32,7 +32,7 @@ const PostGrid: React.FC<GridProps> = ({ unvotedPosts, allPosts, userId }) => {
                             checked={isChecked}
                             onChange={handleChange}
                         />
-                        Show all posts
+                        Show all posts { isChecked ? 'checked' : 'unchecked' }
                         </p>
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
